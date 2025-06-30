860075a3cfc6acefdab7cf56892d531edfaa0e28
Ashwin5641 <tic1@hhvthermaltech.com>
2025-06-30 17:11:44 +0530

    Updated

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="footer-links-content">
+        <div class="logo-mail-icons">
+            <div class="logo">
+                <a href="./assets/images/logo.png"></a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ topbar.php
@@ -0,0 +1,15 @@
+<div class="topbar">
+    <div class="location">
+        <ul>
+            <li><i class="fa-solid fa-location-dot"></i><span>&nbsp;&nbsp;No. 17, Phase 1, Peenya, Bengaluru, India</span></li>
+        </ul>
+    </div>
+    <div class="media-icons">
+        <ul>
+            <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+        </ul>
+    </div>
+</div>
